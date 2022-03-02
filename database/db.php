0aefa3304049e1b451c93cefb3adb164b800ea59
hamzaatmaca <84020582+hamzaatmaca@users.noreply.github.com>
2022-03-02 22:38:18 +0300

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,6 +1,6 @@
 <?php 
 
-$db = mysqli_connect("localhost","root","","bycnroot");
+$db = mysqli_connect("localhost","******","","*********");
 mysqli_set_charset($db,"utf8");
 if(!$db){
 	echo "Bağlantı Başarısız. Hata: " . $mysqli->connect_error;
